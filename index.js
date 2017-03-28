9f943511a63fbac813c4f3f9b8c238fd6a2dcc18
nivedha1 <r.nivedha1@gmail.com>
2017-03-28 19:48:56 +0530

    User schema added

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 var express = require('express');
 var app = express();
 var request = require('request');
+var User = require('./user');
 app.set('port', (process.env.PORT || 5000));
 
 
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
-
+var mongoose = require('mongoose');
+Promise = require('bluebird');
+Promise.promisifyAll(mongoose);
 // Connection URL
 var url = 'mongodb://nivedha:password@ds145100-a0.mlab.com:45100,ds145100-a1.mlab.com:45100/smartwomen?replicaSet=rs-ds145100'
 // Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server"); 
-  db.close();
+
+
+mongoose.connect(url, function() {
+
+          var newUser = User({
+            name: 'Peter Quill12',
+            username: 'starlord5512',
+            password: 'password12',
+            admin: true
+          });
+          // save the user
+          newUser.save(function(err) {
+            if (err) throw err;
+
+            console.log('User created!');
+              mongoose.connection.close();
+          });// The collection exists
+
+
+
+
+
 });
