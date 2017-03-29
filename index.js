0048c664f1268e346432fe0c2a7ef2f835b63847
nivedha1 <r.nivedha1@gmail.com>
2017-03-29 18:14:44 +0530

    login functionality added

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ var express = require('express');
 var app = express();
 var request = require('request');
 var User = require('./user');
+var PhoneNo = require('./phoneno');
 app.set('port', (process.env.PORT || 5001));
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
 
 var mongoose = require('mongoose');
@@ -14,24 +15,85 @@ Promise.promisifyAll(mongoose);
 var url = 'mongodb://nivedha:password@ds145100-a0.mlab.com:45100,ds145100-a1.mlab.com:45100/smartwomen?replicaSet=rs-ds145100'
 // Use connect method to connect to the Server
 app.get('/', function(req, res) {
-  console.log('app running successfully')
+    console.log('app running successfully')
 });
-app.get('/createuser',function(req,res){
-mongoose.connect(url, function() {
+app.get('/createuser', function(req, res) {
+    mongoose.connect(url, function() {
+        User.find({
+                'username': req.query.username
+            }, function(err, docs) {
+              if(err)
+              {
+                console.log(err);
+                mongoose.connection.close();
+                res.send("failure");
+              }else if (docs.length == 0) {
+                    var newUser = User({
+                        name: req.query.name,
+                        username: req.query.username,
+                        password: req.query.password,
+                    });
+                    newUser.save(function(err) {
+                        if (err) {
+                          console.log(err);
+                          mongoose.connection.close();
+                          res.send("failure");
+                        }
+                        else {
+                            console.log('User created!');
+                            mongoose.connection.close();
+                            res.send("success");
+                        }
+                    });
+                } else {
+                  console.log('User not created!');
+                    mongoose.connection.close();
+                    res.send("failure");
+                }
+            });
+    });
+});
 
-          var newUser = User({
-            name: req.query.name,
-            username: req.query.name,
-            password: req.query.password,
-          });
-          // save the user
-          newUser.save(function(err) {
-            if (err) throw err;
-            else{
-            console.log('User created!');
-            res.send("created");
-          }
-              mongoose.connection.close();
-          });// The collection exists
+app.get('/login', function(req, res) {
+    mongoose.connect(url, function() {
+        if (User.find({
+                username: req.query.username,
+                password: req.query.password
+            }, function(err, docs) {
+              if(err)
+              {
+                console.log(err);
+                  mongoose.connection.close();
+                res.send("failure");
+              }else if (docs.length == 0) {
+                    console.log('Username doest exists!');
+                    mongoose.connection.close();
+                    res.send("failure");
+                } else {
+                    console.log('Username exists!');
+                    mongoose.connection.close();
+                    res.send("success");
+                }
+            }));
+    });
 });
+app.get('/addphoneno', function(req, res) {
+    mongoose.connect(url, function() {
+        var phoneNos = PhoneNo({
+            name: req.query.name,
+            daycare_no: req.query.daycareno,
+            forward_no: req.query.forwardno,
+            lateforward_no: req.query.lateforwardno,
+            meeting_time: req.query.meetingtime
+        });
+        // save the user
+        phoneNos.save(function(err) {
+            if (err) throw err;
+            else {
+                console.log('Phone nos saved');
+                res.send("created");
+            }
+            mongoose.connection.close();
+        }); // The collection exists
+    });
 });
---
+++ phoneno.js
@@ -0,0 +1,21 @@
+// grab the things we need
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// create a schema
+var phoneNumberSchema = new Schema({
+  username: { type: String, required: true, unique: true},
+  daycare_no:String,
+  forward_no: String,
+  lateforward_no: String,
+  created_at: Date,
+  updated_at: Date,
+  meeting_time:Date
+});
+
+// the schema is useless so far
+// we need to create a model using it
+var PhoneNo = mongoose.model('PhoneNo', phoneNumberSchema);
+
+// make this available to our users in our Node applications
+module.exports = PhoneNo;
