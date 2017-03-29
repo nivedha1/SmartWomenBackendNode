78001c3cd511ef97ce763f3fec7ba953acfe3a21
nivedha1 <r.nivedha1@gmail.com>
2017-03-29 08:42:50 +0530

    User schema added

--- index.js
+++ index.js
@@ -14,20 +14,21 @@ Promise.promisifyAll(mongoose);
 var url = 'mongodb://nivedha:password@ds145100-a0.mlab.com:45100,ds145100-a1.mlab.com:45100/smartwomen?replicaSet=rs-ds145100'
 // Use connect method to connect to the Server
 
-app.ge
+app.get('createuser',function(req,res){
 mongoose.connect(url, function() {
 
           var newUser = User({
-            name: 'Peter Quill12',
-            username: 'starlord5512',
-            password: 'password12',
-            admin: true
+            name: req.query.name,
+            username: req.query.name,
+            password: req.query.password,
           });
           // save the user
           newUser.save(function(err) {
             if (err) throw err;
-
+            else{
             console.log('User created!');
+            res.send("created");
+          }
               mongoose.connection.close();
           });// The collection exists
 
